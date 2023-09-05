50dd6e5eebecbcfb5da74c065005e8cf2019faf3
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2023-09-05 18:08:56 -0500

    solved taks if-else and loops

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for numero in range(0, 99):
+    print(f'{numero} = {hex(numero)}')
---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for numero in range(0, 99):
+    print(f'{numero:02}, ', end='')
+print(99)
