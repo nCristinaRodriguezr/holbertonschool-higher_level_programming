c20250aff99127b32a1500bf64d29882f0e32774
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2023-09-08 20:03:00 -0500

    task 5 and 100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,9 +5,10 @@ my_operations = {'+': add, '-': sub, '*': mul, '/': div}
 if len(sys.argv)>4 or len(sys.argv)<4:
     print ('Usage: ./100-my_calculator.py <a> <operator> <b>')
     exit(1)
-elif sys.argv[2] != '+' or sys.argv[2] != '-' or sys.argv[2] != '*' or sys.argv[2] != '/':
+elif sys.argv[2] != '+' and sys.argv[2] != '-' and sys.argv[2] != '*' and sys.argv[2] != '/':
     print('Unknown operator. Available operators: +, -, * and /')
     exit(1)
 else:
     result = my_operations[sys.argv[2]](int(sys.argv[1]), int(sys.argv[3]))
     print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {result}')
+    exit(0)
