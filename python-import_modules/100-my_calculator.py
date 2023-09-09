6dc0a89c3e05cab17b677fbd4974cc0de13205c4
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2023-09-08 19:54:27 -0500

    task 5 and 100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+my_operations = {'+': add, '-': sub, '*': mul, '/': div}
+if len(sys.argv)>4 or len(sys.argv)<4:
+    print ('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    exit(1)
+elif sys.argv[2] != '+' or sys.argv[2] != '-' or sys.argv[2] != '*' or sys.argv[2] != '/':
+    print('Unknown operator. Available operators: +, -, * and /')
+    exit(1)
+else:
+    result = my_operations[sys.argv[2]](int(sys.argv[1]), int(sys.argv[3]))
+    print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {result}')
--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import hidden_4
 for name in dir(hidden_4):
     if name[0:2] != '__':
