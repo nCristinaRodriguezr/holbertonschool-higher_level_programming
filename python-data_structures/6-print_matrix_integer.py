e422dca690da580996d16ef6bb857e98527888bd
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-09-11 16:39:30 -0700

    fix of task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for fila in matrix:
-        for i in range(len(fila)):
-            if i == len(fila)-1:
-                print(f'{fila[i]}')
-            else:
-                print(f'{fila[i]}', end = " ")
+    if len(matrix[0])>0:
+        for fila in matrix:
+            for i in range(len(fila)):
+                if i == len(fila)-1:
+                    print(f'{fila[i]}')
+                else:
+                    print(f'{fila[i]}', end = " ")
+    else:
+        print('')
