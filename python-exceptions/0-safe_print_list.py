f8192fbfd004615f99acfcdb434754ff2fea2c69
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2023-09-20 19:30:20 -0500

    solve task 0- 102 exceptions

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    element_print = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            element_print = element_print + 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return element_print
