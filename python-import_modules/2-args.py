06dd532c7b7a639d10e2a6b801c62aacaf0fab9d
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-12 10:09:10 -0500

    fix two points

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print('1 argument:')
     else:
-        print('{}: arguments'.format(len(sys.argv) - 1))
+        print('{} arguments:'.format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print('{}: {}'.format(i, sys.argv[i]))
