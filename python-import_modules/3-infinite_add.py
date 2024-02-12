f017d29fc6e3335e5ad8ef38e4d132b3d8f609aa
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-11 19:31:14 -0500

    fix with .format

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,10 +3,10 @@ import sys
 from add_0 import add
 if __name__ == "__main__":
     result = 0
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         print('0')
     else:
         for i in range(1, len(sys.argv)):
             numero = int(sys.argv[i])
             result = add(result, numero)
-        print(f'{result}')
+        print('{}'.format(result))
