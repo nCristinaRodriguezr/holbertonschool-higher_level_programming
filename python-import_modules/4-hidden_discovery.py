fc0d85424ff0455e67785aca938d2a71b149aab1
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-12 10:28:58 -0500

    fix last line

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     for i, name in enumerate(dir(hidden_4)):
         if name[0:2] != '__':
             if i == len(dir(hidden_4))-1:
-                print (name, end=" ")
+                print(name, end=" ")
             else:
                 print(name)
