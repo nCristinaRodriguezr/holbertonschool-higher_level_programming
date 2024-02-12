8a6adca03a73c9bdd9e1a37a4d6ca596fb59c563
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-11 19:23:56 -0500

    fix with .format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif len(sys.argv)==2:
         print('1 argument:') 
     else:
-        print(f'{len(sys.argv)-1}: arguments')
+        print('{}: arguments'.format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
-        print(f'{i}: {sys.argv[i]}')
+        print('{}: {}'.format(i, sys.argv[i]))
