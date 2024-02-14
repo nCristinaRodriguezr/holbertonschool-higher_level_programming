79d8b7148cfd9e05b2a363bd7f5bd79aff40f722
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-14 13:55:44 -0500

    fix Return none

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return None
     length = len(sentence)
+    if length == 0:
+        return (length, None)
     first_char = sentence[0]
     return (length, first_char)
