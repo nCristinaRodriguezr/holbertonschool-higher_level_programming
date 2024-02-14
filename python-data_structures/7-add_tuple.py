a6dd823ec9ec68e0854f4149b12e72cd42e6efb2
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-14 12:46:00 -0500

    fix pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()): 
+def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 0:
         copy_tuple_a = (0, 0)
     elif len(tuple_a) == 1:
@@ -13,6 +13,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         copy_tuple_b = tuple_b + (0,)
     else:
         copy_tuple_b = tuple_b
-   
-    suma_tuples = (copy_tuple_a[0] + copy_tuple_b[0], copy_tuple_a[1] + copy_tuple_b[1])
+    suma_tuples = (copy_tuple_a[0] + copy_tuple_b[0],
+                   copy_tuple_a[1] + copy_tuple_b[1])
     return suma_tuples
