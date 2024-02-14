125ccf7d04f9632bdb3459d9a62c6560133a52bc
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-14 15:22:57 -0500

    fix the last line

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):    
+def best_score(a_dictionary):
     if not a_dictionary:
         return None
     key_max = max(a_dictionary, key=lambda key: a_dictionary[key])
