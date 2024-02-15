54122c82b853a5ef4cbafe5894a37ba8d38919ac
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-15 14:08:26 -0500

    fix lest length

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
-    for i in range(max(len(my_list_1), len(my_list_2))):
+    for i in range(0, list_length):
         try:
             result_list.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             result_list.append(0)
-            print ("division by 0")
+            print("division by 0")
         except TypeError:
             result_list.append(0)
-            print ("wrong type")
+            print("wrong type")
         except IndexError:
             result_list.append(0)
-            print ("out of range")
+            print("out of range")
         finally:
             pass
     return result_list
-        
