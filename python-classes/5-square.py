f351c7b397a5f4b2d9462d3dcf09cebf34caa9f2
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-15 17:02:36 -0500

    documentation and style

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""
+class Square that defines a square by: (based on 4-square.py)
+"""
+
+
 class Square:
     """
-    The objective of this class is to learn how to assign a method that changes a private attribute
+    The objective of this class is to learn how to assign a method that
+    changes a private attribute
     """
     def __init__(self, size=0):
         self.__size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -15,12 +23,13 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size * self.__size
+
     def my_print(self):
         if self.__size == 0:
             print()
         else:
             for i in range(self.__size):
                 print("#" * self.__size)
-    
