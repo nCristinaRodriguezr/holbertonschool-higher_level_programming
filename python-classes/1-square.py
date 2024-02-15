30c99828148d9e2e0de79261b4d02a0691cc6b6f
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-15 16:18:49 -0500

    documentation and style

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""
+making classes in this file
+"""
+
+
 class Square:
     """
     The objetive of this class is learning to do a private instance attribute.
     """
     __size = 0
-    
+
     def __init__(self, size):
         self.__size = size
