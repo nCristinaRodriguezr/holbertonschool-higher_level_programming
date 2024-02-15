6de77772c6f011bf275bbee6c5a30a868e5d934e
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-15 16:42:15 -0500

    documentation and style

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""
+defining the class a square
+"""
+
+
 class Square:
     """
     The objetive of this class is learning to use methods.
     """
     __size = None
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-            
-        
