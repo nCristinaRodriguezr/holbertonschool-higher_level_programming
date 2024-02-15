34f1b1aca38cca9867cfe2208e355f185ab378e5
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-15 12:25:17 -0500

    fix spaces

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     element_print = 0
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             element_print = element_print + 1
     except IndexError:
         pass
