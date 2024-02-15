bfc56c63277dbac123f02c68693ee965d8196475
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-15 12:44:11 -0500

    fix style

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,10 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     element_print = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end= "")
+            print("{:d}".format(my_list[i]), end="")
             element_print = element_print + 1
         except (ValueError, TypeError):
             pass
     print()
     return element_print
-            
