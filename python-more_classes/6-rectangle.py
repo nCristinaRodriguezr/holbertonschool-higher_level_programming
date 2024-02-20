bcef7388c3b1830f8210cc274f876419dc6faef6
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-20 18:55:10 -0500

    fix pycodestyle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
+"""
+a class Rectangle that defines a rectangle by: (based on 5-rectangle.py)
+"""
+
+
 class Rectangle:
     """
-    This class aims to create a triangular object and define its width and height,
-    as well as multiply the base with the height to get the area of 
+    This class aims to create a triangular
+    object and define its width and height,
+    as well as multiply the base with the
+    height to get the area of
     the rectangle and represent the rectangle with #
     Args : width, height
     """
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.__height = 0
+        self.__width = 0
+        self.height = height
+        self.width = width
         Rectangle.number_of_instances = Rectangle.number_of_instances + 1
 
     @property
     def width(self):
         return self.__width
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -28,7 +37,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -39,12 +48,12 @@ class Rectangle:
 
     def area(self):
         return self.__height * self.__width
-    
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return None
         return (self.__height * 2) + (self.__width * 2)
-    
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
@@ -52,11 +61,10 @@ class Rectangle:
         for i in range(self.__height):
             rectangle_str = rectangle_str + "#" * self.width + "\n"
         return rectangle_str.rstrip()
-    
+
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
-    
+
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances = Rectangle.number_of_instances - 1
-
