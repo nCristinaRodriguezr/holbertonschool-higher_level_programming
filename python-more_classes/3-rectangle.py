5420672b55a08a71d9edc7a874096285aa3b800d
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-20 14:47:42 -0500

    fix pycodestyle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
+"""
+A class Rectangle that defines a rectangle by: (based on 2-rectangle.py)
+"""
+
+
 class Rectangle:
     """
-    This class aims to create a triangular object and define its width and height,
-    as well as multiply the base with the height to get the area of 
+    This class aims to create a triangular object
+    and define its width and height, as well as multiply
+    the base with the height to get the area of
     the rectangle and represent the rectangle with #
     Args : width, height
     """
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.__height = 0
+        self.__width = 0
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
         return self.__width
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -25,7 +33,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -36,12 +44,12 @@ class Rectangle:
 
     def area(self):
         return self.__height * self.__width
-    
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return None
         return (self.__height * 2) + (self.__width * 2)
-    
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
@@ -49,6 +57,3 @@ class Rectangle:
         for i in range(self.__height):
             rectangle_str = rectangle_str + "#" * self.width + "\n"
         return rectangle_str.rstrip()
-
-    
-  
