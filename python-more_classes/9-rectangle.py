d49a4134dc9e686428e0b7753e4e6121d6363268
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-02-21 15:36:37 -0500

    fix many things

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -17,10 +17,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.__height = 0
-        self.__width = 0
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances = Rectangle.number_of_instances + 1
 
     @property
@@ -52,7 +50,7 @@ class Rectangle:
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
-            return None
+            return 0
         return (self.__height * 2) + (self.__width * 2)
 
     def __str__(self):
