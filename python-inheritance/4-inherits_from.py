30dd14b55b360af161a896b0cb166cf5c777d1fe
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-03-01 10:58:56 -0500

    point 4 answer

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     """
     This function have a return
     """
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
