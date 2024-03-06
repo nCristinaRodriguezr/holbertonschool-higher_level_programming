89a90bc6cce64d21b75c05f726b2ef534e4604b7
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-03-05 19:52:11 -0500

    point 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -15,7 +15,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """
         Retrieves a dictionary representation of a Student instance
