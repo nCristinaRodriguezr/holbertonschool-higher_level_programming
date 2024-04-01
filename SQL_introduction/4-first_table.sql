5ded1e716ea4f08a35564c6df427a3bc97d019ed
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-03-31 21:20:59 -0500

    point 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,10 @@
--- a script that creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VACHAR(256),
-);
+-- Establecer el nombre de la base de datos como una variable de usuario
+SET @db_name := DATABASE();
+
+-- Construir la consulta para crear la tabla si no existe
+SET @create_table_query = CONCAT('CREATE TABLE IF NOT EXISTS ', @db_name, '.first_table (id INT, name VARCHAR(256));');
+
+-- Ejecutar la consulta
+PREPARE stmt FROM @create_table_query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
