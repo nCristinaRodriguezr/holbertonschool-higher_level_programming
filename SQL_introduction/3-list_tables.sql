2d66cccd1746ef288dd58b1888f6bbc3fbb52382
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-03-31 21:02:46 -0500

    point 3

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,10 @@
--- a script that lists all the tables of a database in your MySQL server.
-SELECT table_name AS 'Tables_in_mysql'
-FROM information_schema.tables
-WHERE table_schema = DATABASE();
+-- Establecer el nombre de la base de datos como una variable de usuario
+SET @db_name := DATABASE();
+
+-- Construir la consulta para listar las tablas
+SET @query = CONCAT('SELECT table_name AS `Tables_in_', @db_name, '` FROM information_schema.tables WHERE table_schema = "', @db_name, '"');
+
+-- Ejecutar la consulta
+PREPARE stmt FROM @query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
 -- a script that creates a table called first_table in the current database in your MySQL server.
-USE DATABASE();
 CREATE TABLE IF NOT EXISTS first_table (
     ID INT,
     NAME VACHAR(256),
