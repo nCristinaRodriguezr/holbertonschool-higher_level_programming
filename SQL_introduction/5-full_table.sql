0a73e068ad66efe8b73eee4b1a7f86f889f25f39
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-04-01 10:41:19 -0500

    point 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,12 @@
 -- a script that prints the following description of the
 -- table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SET @db_name := DATABASE();
+
+SET @desc_table_query = CONCAT('
+    SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = ''', @db_name, ''' AND TABLE_NAME = ''first_table'';'
+);
+PREPARE stmt FROM @desc_table_query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
