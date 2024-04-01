668863bf0cded726917ec9e5569224c8e7595b40
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-03-31 19:25:15 -0500

    point 3

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
 -- a script that lists all the tables of a database in your MySQL server.
-SELECT table_name
+SELECT table_name AS Tables_in_mysql
 FROM information_schema.tables
 WHERE table_schema = DATABASE();
