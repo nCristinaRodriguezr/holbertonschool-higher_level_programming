659751fbc52c3d08136f2a25e6ea4e1013064dbc
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-04-02 11:44:57 -0500

    point 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,6 @@
 -- a script that creates the table force_name on your MySQL server.
-USE hbtn_0d_2
 CREATE TABLE IF NOT EXISTS force_name
 (
-    id INT PRIMARY KEY,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
