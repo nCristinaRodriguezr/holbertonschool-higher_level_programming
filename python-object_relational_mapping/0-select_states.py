fa90b9498bcc390900e1b7faa8fd37d0931939b4
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-04-03 13:00:00 -0500

    point 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa:
+"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_pwd = argv[2]
+    db_name = argv[3]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_pwd,
+        db=db_name
+        )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
