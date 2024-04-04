86c2c429a78c72a07f8e875cbe74bc5a9b76e35b
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-04-03 22:11:11 -0500

    point 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         db=db_name
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
